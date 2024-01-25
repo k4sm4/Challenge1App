8ed77eb6458a6cbd0d8fb148738b6c6302d95f9f
k4sm4 <marekkosmowski@yahoo.com>
2024-01-25 13:25:13 +0000

    zadanie domowe dzien 4 poprawione nazwy zmiennych na angielskie (camelCase)

--- Challenge1App/Challenge1App/Program.cs
+++ Challenge1App/Challenge1App/Program.cs
@@ -1,16 +1,16 @@
-﻿var NAME = "Marek";
-var SEX = "mezczyzna";
-var AGE = 40;
+﻿var name = "Marek";
+var sex = "mezczyzna";
+var age = 40;
 
-if (SEX == "kobieta" && AGE < 30)
+if (sex == "kobieta" && age < 30)
 {
     Console.WriteLine("Kobieta ponizej 30 lat");
 }
-else if (NAME == "Ewa" && AGE == 30)
+else if (name == "Ewa" && age == 30)
 {
     Console.WriteLine("Ewa, lat 30");
 }
-else if (SEX == "mezczyzna" && AGE < 18)
+else if (sex == "mezczyzna" && age < 18)
 {
     Console.WriteLine("Niepelnoletni mezczyzna");
 }
