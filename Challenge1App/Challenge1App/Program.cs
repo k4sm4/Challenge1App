f10efd822956c65e82673870cf1bdd404a8c455e
k4sm4 <marekkosmowski@yahoo.com>
2024-01-25 03:01:02 +0000

    Dzien 4 Zadanie domowe

--- Challenge1App/Challenge1App/Program.cs
+++ Challenge1App/Challenge1App/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Siema byku!");
+﻿var imie = "Marek";
+var plec = "mezczyzna";
+var wiek = 40;
+
+if (plec == "kobieta" && wiek < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (imie == "Ewa" && wiek == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (plec == "mezczyzna" && wiek < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
+else
+{
+    Console.WriteLine("NIC");
+}
