21ed6b163e5b51a490745b58a811d7ab2f1c86ce
k4sm4 <marekkosmowski@yahoo.com>
2024-01-29 15:20:27 +0000

    Digit counter

--- Challenge1App/Challenge1App/Program.cs
+++ Challenge1App/Challenge1App/Program.cs
@@ -1,20 +1,74 @@
-﻿var name = "Marek";
-var sex = "mezczyzna";
-var age = 40;
+﻿using System.Diagnostics.Metrics;
 
-if (sex == "kobieta" && age < 30)
+int number = 154864;
+string numberAsString = number.ToString();
+char[] digit = numberAsString.ToArray();
+
+Console.WriteLine("The result for number " + number);
+Console.WriteLine("");
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+foreach (char letter in digit)
 {
-    Console.WriteLine("Kobieta ponizej 30 lat");
+    if (letter == '0')
+    { 
+        counter0++;
+    }
+    else if(letter == '1')
+    {
+        counter1++;
+    }
+    else if(letter == '2')
+    {
+        counter2++;
+    }
+    else if(letter == '3')
+    {
+        counter3++;
+    }
+    else if(letter == '4')
+    {
+        counter4++;
+    }
+    else if(letter == '5')
+    {
+        counter5++;
+    }
+    else if(letter == '6')
+    {
+        counter6++;
+    }
+    else if(letter == '7')
+    {
+        counter7++;
+    }
+    else if(letter == '8')
+    {
+        counter8++;
+    }
+    else if(letter == '9')
+    {
+        counter9++;
+    }
 }
-else if (name == "Ewa" && age == 30)
-{
-    Console.WriteLine("Ewa, lat 30");
-}
-else if (sex == "mezczyzna" && age < 18)
-{
-    Console.WriteLine("Niepelnoletni mezczyzna");
-}
-else
-{
-    Console.WriteLine("NIC");
-}
+Console.WriteLine("Digi 0 was used = " + counter0);
+Console.WriteLine("Digi 1 was used = " + counter1);
+Console.WriteLine("Digi 2 was used = " + counter2);
+Console.WriteLine("Digi 3 was used = " + counter3);
+Console.WriteLine("Digi 4 was used = " + counter4);
+Console.WriteLine("Digi 5 was used = " + counter5);
+Console.WriteLine("Digi 6 was used = " + counter6);
+Console.WriteLine("Digi 7 was used = " + counter7);
+Console.WriteLine("Digi 8 was used = " + counter8);
+Console.WriteLine("Digi 9 was used = " + counter9);
+
+Console.ReadLine();
